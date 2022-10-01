a94910ab95c123e33ced6df89df25e796dcdeb00
arunesh-singh <aruneshsingh433@gmail.com>
2022-10-02 00:16:48 +0530

    postfix evaluation
    Signed-off-by: arunesh-singh <aruneshsingh433@gmail.com>

---
+++ OOPS/Queue/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Queue
+{
+
+    int front, back;
+    int capacity, size;
+    int *array;
+
+public:
+    Queue(int lenght)
+    {
+        capacity = lenght;
+        front = back = size = 0;
+        array = new int[capacity];
+    }
+
+    void enQueue(int data)
+    {
+        if ()
+    }
+};
