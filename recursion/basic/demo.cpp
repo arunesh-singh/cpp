573ee93d38e371bed115cb6fa75faedd0243040b
arunesh-singh <aruneshsingh433@gmail.com>
2025-08-15 09:15:24 +0530

    formated
    Signed-off-by: arunesh-singh <aruneshsingh433@gmail.com>

--- recursion/basic/demo.cpp
+++ recursion/basic/demo.cpp
@@ -4,10 +4,9 @@
 #include <algorithm>
 using namespace std;
 
-
-
-int main(){
-    int array[]={1,2,3,4,5};
-    cout<<array+1<<endl;
-    return 0;
+int main()
+{
+  int array[] = {1, 2, 3, 4, 5};
+  cout << array + 1 << endl;
+  return 0;
 }
